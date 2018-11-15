f30f7dc45fddcdbdbb7a8ba66d4fdac9aa8b51d3
destiny <destinywk@163.com>
2018-11-15 17:58:38 +0800

    10_commaWithBuff.go

--- ch3/5_string/exercises/10_commaWithBuff.go
+++ ch3/5_string/exercises/10_commaWithBuff.go
@@ -1,6 +1,24 @@
-
+// 练习 3.10
+// 编写一个非递归的 comma 函数, 运用 bytes.Buffer, 而不是简单的字符串拼接
 package main
 
+import (
+	"bytes"
+	"fmt"
+)
+
+func comma(s string) string {
+	var buff bytes.Buffer
+	l := len(s)
+	for i := 0; i < l; i++ {
+		buff.WriteByte(s[i])
+		if (l-i-1)%3 == 0 && i != l-1 {
+			buff.WriteByte(',')
+		}
+	}
+	return buff.String()
+}
+
 func main() {
-	
+	fmt.Println(comma("12345"))
 }
